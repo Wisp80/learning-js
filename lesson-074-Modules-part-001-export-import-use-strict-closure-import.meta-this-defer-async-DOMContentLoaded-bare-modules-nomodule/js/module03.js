bb78f563dde5f8982cbe657c5411f776e1f94be2
Wisp <therearewispseverywhere@gmail.com>
2024-09-15 23:29:53 +0300

    lesson 074 Modules part 001: export, import, use strict, closure, import.meta, this, defer, async,         DOMContentLoaded, bare modules, nomodule

---
+++ lesson-074-Modules-part-001-export-import-use-strict-closure-import.meta-this-defer-async-DOMContentLoaded-bare-modules-nomodule/js/module03.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+
+// console.log(' ');
+// console.log('--- we are in module03.js ---');
+
+// import { obj01FromModule04 } from './module04.js';
+
+// console.log(obj01FromModule04.b);
